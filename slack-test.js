936a4615c3976d184af54159755d55e8dbaefd99
Stacey Shkuratoff <sshkuratoff@popsugar.com>
2016-08-15 21:04:49 -0700

    Added Thumbnails to photo page

--- slack-test.js
+++ slack-test.js
@@ -1,24 +1,25 @@
 var photosToDisplay = [];
 
-var flickrPhotoSetUrl = 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos'
-  +'&api_key=30ef710092abe7a2283d14e000aa57f4&photoset_id=72157641470493775&user_id=118690346%40N08'
-  +'&format=json&nojsoncallback=1&auth_token=72157671571015091-175567590ea4bb84&api_sig=6d1c45c8929c9b3f4e551d04d42db351';
+var flickrPhotoSetUrl = 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=d06a651de2c308348be5326769d1ce47&photoset_id=72157641470493775&user_id=118690346%40N08&format=json&nojsoncallback=1&auth_token=72157672512471285-549666d282c2801a&api_sig=eb57fa32e171a8a4db3f0ac66f086a57';
 getJSONForRequest(flickrPhotoSetUrl, getPhotoInfo, console.log);
 
 function getPhotoInfo(json) {
   var photoJson = json.photoset.photo;
 
   for (var i = 0; i < photoJson.length; i++) {
-    var flickrImageInfoUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&'
-      +'api_key=30ef710092abe7a2283d14e000aa57f4&photo_id={id}&format=json&nojsoncallback=1'
+    var flickrImageInfoUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=d06a651de2c308348be5326769d1ce47&photo_id={id}&format=json&nojsoncallback=1'
       .replace('{id}', photoJson[i].id);
 
-      getJSONForRequest(flickrImageInfoUrl, addImageList);
+      getJSONForRequest(flickrImageInfoUrl, addPhotoToList);
   }
 }
 
-function addImageList() {
-  
+function addPhotoToList(photoInfoJson) {
+  var mainSection = document.getElementById('photo-section');
+  var newImg = document.createElement("img");
+  // Get the thumbnail size
+  newImg.setAttribute("src", photoInfoJson.sizes.size[3].source);
+  mainSection.appendChild(newImg);
 }
 
 function getJSONForRequest(url, success, error) {
