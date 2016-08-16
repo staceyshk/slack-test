223c79b4bc7be79767a5aa26399364b27f60a860
Stacey Shkuratoff <sshkuratoff@popsugar.com>
2016-08-16 09:21:35 -0700

    Fixing auth token to be public

--- slack-test.js
+++ slack-test.js
@@ -1,22 +1,29 @@
 var photoListing = [];
 
-var flickrPhotoSetUrl = 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=d06a651de2c308348be5326769d1ce47&photoset_id=72157641470493775&user_id=118690346%40N08&format=json&nojsoncallback=1&auth_token=72157672512471285-549666d282c2801a&api_sig=eb57fa32e171a8a4db3f0ac66f086a57';
+var flickrPhotoSetUrl = 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=f596922296cb3040def0a5b0cdd3eb7a&photoset_id=72157641470493775&user_id=118690346%40N08&format=json&nojsoncallback=1';
 getJSONForRequest(flickrPhotoSetUrl, getPhotoInfo, console.log);
 
 function getPhotoInfo(json) {
   var photoJson = json.photoset.photo;
 
   for (var i = 0; i < photoJson.length; i++) {
-    var flickrImageInfoUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=d06a651de2c308348be5326769d1ce47&photo_id={id}&format=json&nojsoncallback=1'
-      .replace('{id}', photoJson[i].id);
-
-      getJSONForRequest(flickrImageInfoUrl, addPhotoToList, photoJson[i].id);
+    var flickerInfoUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=f596922296cb3040def0a5b0cdd3eb7a&photo_id=12759547784&format=json&nojsoncallback=1';
+    getJSONForRequest(flickerInfoUrl, addPhotoInfo, photoJson[i].id);   
   }
+}
+
+function addPhotoInfo(photoInfoJson, photoId) {
+  photoListing.push({photoId: photoId, photoInfo: photoInfoJson});
+
+  var flickrImageSizesUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=f596922296cb3040def0a5b0cdd3eb7a&photo_id={id}&format=json&nojsoncallback=1'
+      .replace('{id}', photoId); 
+  getJSONForRequest(flickrImageSizesUrl, addPhotoToList, photoId);
 }
 
 function addPhotoToList(photoSizesJson, photoId) {
   // Add photos to array to load larger images later
-  photoListing.push({photoId: photoId, photoSizes: photoSizesJson});
+  var photoObj = findPhotoObj(photoId)
+  photoObj.photoSizes = photoSizesJson;
 
   var mainSection = document.getElementById('photo-section');
   var newSpan = document.createElement('span');
@@ -24,6 +31,11 @@ function addPhotoToList(photoSizesJson, photoId) {
   var newImg = document.createElement('img');
   // Get the thumbnail size
   newImg.setAttribute('src', photoSizesJson.sizes.size[3].source);
+
+  var pNode = document.createElement('p');
+  var textnode = document.createTextNode(photoObj.photoInfo.photo.title._content);  
+  pNode.appendChild(textnode);
+  newSpan.appendChild(pNode);  
   var input = document.createElement('input');
   input.setAttribute('type', 'hidden');
   input.setAttribute('name', 'id');
@@ -37,11 +49,17 @@ function addPhotoToList(photoSizesJson, photoId) {
 function testImgClick(event) {
   var photoId = event.target.nextSibling.value;
   var largerImg = document.getElementById('overlay-img');
-  largerImg.setAttribute('src', findPhotoSizes(photoId).sizes.size[6].source);
-  
+  var photoObj = findPhotoObj(photoId);
+  // Get Medium size photo
+  largerImg.setAttribute('src', photoObj.photoSizes.sizes.size[6].source);
+  // It takes a bit of time to show the image
+  setTimeout(showLightbox, 500);
+}
+
+function showLightbox() {
   var el = document.getElementById('overlay');
 	el.style.visibility = 'visible';
-  el.onclick = closeLightbox;
+  document.getElementById('close').onclick = closeLightbox;
 }
 
 function closeLightbox() {
@@ -49,10 +67,10 @@ function closeLightbox() {
 	el.style.visibility = 'hidden';
 }
 
-function findPhotoSizes(photoId) {
+function findPhotoObj(photoId) {
   for (var i = 0; i < photoListing.length; i++) {
     if (photoListing[i].photoId === photoId) {
-      return photoListing[i].photoSizes;
+      return photoListing[i];
     }
   }
 
@@ -60,12 +78,12 @@ function findPhotoSizes(photoId) {
 }
 
 //optional param id for any object you want embedded
-function getJSONForRequest(url, success, arg) {
+function getJSONForRequest(url, success, objId) {
    var xhr = new XMLHttpRequest();
    xhr.onreadystatechange = function () {
     if (xhr.readyState === 4) {
       if (xhr.status === 200) {
-        success(JSON.parse(xhr.responseText), arg);
+        success(JSON.parse(xhr.responseText), objId);
       } else {
         error(xhr.responseText);
       }
